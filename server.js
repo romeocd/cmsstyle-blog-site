38a7b5c7a7c98cf7a80ae95b4f1ac651cb664d80
romeo <rdumlao07@gmail.com>
2023-11-30 19:57:37 -0500

    update main.handlebars and server.js

--- server.js
+++ server.js
@@ -1,56 +1,46 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const bodyParser = require('body-parser');
-const path = require('path');
-const dotenv = require('dotenv');
-const sequelize = require('./config/connection'); // Your Sequelize connection
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// Load environment variables from .env file
-dotenv.config();
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
-// Static folder
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Session middleware using Sequelize for session store
-const sessionStore = new SequelizeStore({
-  db: sequelize,
-});
+app.use(routes);
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'secret',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-// View engine setup (assuming you're using Handlebars)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs'); // Assuming you're using hbs files for views
-
-// Your routes setup (these are example routes)
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const postRouter = require('./routes/post');
-const userRouter = require('./routes/user');
-
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
-app.use('/posts', postRouter);
-app.use('/users', userRouter);
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
