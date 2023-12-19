81c6c4ba637bfb22d1e8df0630bae69a0695dd2d
romeo <rdumlao07@gmail.com>
2023-12-18 21:25:05 -0500

    update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,58 @@
-const express = require('express');
-const { getHomePage, getDashboardPage } = require('../controllers/homeController');
-const router = express.Router();
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-// Route for the homepage
-router.get('/', getHomePage);
+// Route to render the homepage with all blog posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{ model: User, attributes: ['username'] }]
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('homepage', { posts, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// Route for the user's dashboard
-router.get('/dashboard', getDashboardPage);
+// Route to render a single blog post by ID
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User, attributes: ['username'] }]
+        });
+        if (postData) {
+            const post = postData.get({ plain: true });
+            res.render('post', { ...post, logged_in: req.session.logged_in });
+        } else {
+            res.status(404).json({ message: 'No post found with this id!' });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route to render the user's dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }]
+        });
+        const user = userData.get({ plain: true });
+        res.render('dashboard', { ...user, logged_in: true });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for user login
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
 
 module.exports = router;
