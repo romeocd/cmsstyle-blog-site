a4f59cae38391d0934bc8e41c9c96d7b282e2a7d
romeo <rdumlao07@gmail.com>
2024-01-01 14:41:06 -0500

    get Route for single user in userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,61 @@
-const express = require('express');
-const { createUser, loginUser, logoutUser } = require('../controllers/userController');
-const router = express.Router();
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/signup', createUser); // Sign up
-router.post('/login', loginUser);    // Login
-router.post('/logout', logoutUser);  // Logout
+//Get all users
+router.get('/', (req,res) => {
+    User.findAll({
+        attributes: { exclude: ['Password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err);
+    });
+});
 
-module.exports = router;
+//Get a single user by ID
+
+router.get('/:id', (req,res) => {
+    User.findOne({
+        attributes: {exclude: ['Password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [{ 
+            model: Post,
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ]
+        },
+        {
+            model: Comment,
+            attributes: [
+                'id', 'comment_text', 'created_at'
+            ],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        },
+        {
+            model: Post,
+            attributes: ['title'],
+        }
+    ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log (err);
+        res.status(500).json(err);
+    });
+});
