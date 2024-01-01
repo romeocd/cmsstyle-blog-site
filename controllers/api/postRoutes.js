76b06437b322058dc9aa5d6f1158fea42ca3cce8
romeo <rdumlao07@gmail.com>
2024-01-01 14:07:07 -0500

    get Route for posts in postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,17 +1,67 @@
-const express = require('express');
-const { 
-  createPost, 
-  getPosts, 
-  getPostById, 
-  updatePost, 
-  deletePost 
-} = require('../controllers/postController');
-const router = express.Router();
+const router = require('express');
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', createPost);       // Create a new post
-router.get('/', getPosts);          // Get all posts
-router.get('/:id', getPostById);    // Get a single post by ID
-router.put('/:id', updatePost);     // Update a post by ID
-router.delete('/:id', deletePost);  // Delete a post by ID
+// Get all posts
+router.get('/', (req, res) => {
+  Post.findAll({
+      attributes: ['id', 'title', 'content', 'created_at'],
+      order: [['created_at', 'DESC']],
+      include: [
+          {
+              model: User,
+              attributes: ['username']
+          },
+          {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                  model: User,
+                  attributes: ['username']
+              }
+          }
+      ]
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+
+// Get a single Post by ID
+router.get('/:id', (req,res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'content', 'title', 'created_at'],
+    include: [
+      {
+        model: User, 
+        attributes: ['username']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No Post found with this id' });
+      return;
+    }
+    res.json(dbPostData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
