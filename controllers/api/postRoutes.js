8c2b09adda267182592e5f1be6ec6286d9c8d240
romeo <rdumlao07@gmail.com>
2024-01-01 14:20:06 -0500

    put route on postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -64,4 +64,28 @@ router.get('/:id', (req,res) => {
   });
 });
 
+
+//Update a post
+router.put('/:id', withAuth, (req,res) => {
+  Post.update({
+    title: req.body.title,
+    content: req.body.content,
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbPostData => {
+    if(!dbPostData) {
+      res.status(404).json({ message: 'No Post found with this id'});
+      return;
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+
 module.exports = router;
