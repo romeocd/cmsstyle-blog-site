f1487ffa25ebeba39bc602c5f05ee266f70ade49
romeo <rdumlao07@gmail.com>
2024-01-10 15:19:44 -0500

    updated some syntax errors

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-//Route to render the user's dashboard
-router.get('/', withAuth, (req, res => {
+// Route to render the user's dashboard
+router.get('/', withAuth, (req, res) => {
     Post.findAll({
         where: {
             user_id: req.session.user_id
@@ -12,34 +12,34 @@ router.get('/', withAuth, (req, res => {
         include: [
             {
                 model: User,
-                attributes:['username']
+                attributes: ['username']
             }
         ]
     })
     .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true}));
+        const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('dashboard', { posts, loggedIn: true });
     })
     .catch(err => {
         console.log(err);
-        res.statusCode(500).json(err);
+        res.status(500).json(err);
     });
-}));
+});
 
-//Route to render the form to add a new post
+// Route to render the form to add a new post
 router.get('/new', withAuth, (req, res) => {
     res.render('new-post', { loggedIn: true });
 });
 
-//Route to render the form to edit an existing post
-router.get('/edit:id', withAuth, (req, res) => {
+// Route to render the form to edit an existing post
+router.get('/edit/:id', withAuth, (req, res) => {
     Post.findByPk(req.params.id)
     .then(dbPostData => {
         if (dbPostData) {
             const post = dbPostData.get({ plain: true });
             res.render('edit-post', { post, loggedIn: true });
         } else {
-            res.status(404).json({ message: 'No post found with this id'});
+            res.status(404).json({ message: 'No post found with this id' });
         }
     })
     .catch(err => {
