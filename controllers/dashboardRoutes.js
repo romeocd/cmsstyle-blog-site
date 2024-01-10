f9397c160d60324f5cc2653702d3449a236df6f2
romeo <rdumlao07@gmail.com>
2024-01-10 15:01:43 -0500

    dashboardRoute render user's dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,3 +2,27 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+//Route to render the user's dashboard
+router.get('/', withAuth, (req,res => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'title', 'content', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.statusCode(500).json(err);
+    });
+}));
+
