d8d00c8e6ece0b0217c8427dd9a3b2b2c0c88632
romeo <rdumlao07@gmail.com>
2024-01-10 15:07:10 -0500

    dashboardRoutes Route to render the form to edit an existing post

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,7 +3,7 @@ const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 //Route to render the user's dashboard
-router.get('/', withAuth, (req,res => {
+router.get('/', withAuth, (req, res => {
     Post.findAll({
         where: {
             user_id: req.session.user_id
@@ -27,7 +27,25 @@ router.get('/', withAuth, (req,res => {
 }));
 
 //Route to render the form to add a new post
-router.get('/new', withAuth, (req,res) => {
+router.get('/new', withAuth, (req, res) => {
     res.render('new-post', { loggedIn: true });
 });
 
+//Route to render the form to edit an existing post
+router.get('/edit:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id)
+    .then(dbPostData => {
+        if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        } else {
+            res.status(404).json({ message: 'No post found with this id'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
